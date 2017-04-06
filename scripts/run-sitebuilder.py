41b84707e8b53ad82821ff271ccfe22eee160e68
Louis Moresi <louis.moresi@gmail.com>
2017-04-06 22:41:39 +1000

    Changes to the assessment tasks

--- scripts/run-sitebuilder.py
+++ scripts/run-sitebuilder.py
@@ -7,14 +7,10 @@ import os
 sitePath = os.path.normpath(os.path.dirname((os.path.join(os.getcwd(),os.path.dirname(__file__) ) ) ) )
 siteDir = os.path.join(sitePath,"www")
 
-# Copy Notebooks to the user space (leaving originals unchanged)
-# Make symlinks for Data and Documentation so that these are visible
-# to the web server started in the www build directory
 
 print "Building {:s}".format(siteDir)
 
 call("cd {:s} && mkdocs build --theme united --clean".format(sitePath), shell=True)
-
 
 uwDocDir = "/underworld/underworld2/docs" # This assumes the unix build
 uwDeployDir = os.path.join(siteDir, "Introduction")
